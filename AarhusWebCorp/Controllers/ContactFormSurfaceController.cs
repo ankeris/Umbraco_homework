e56d9c3e5e43aa64023a8db978bc299e671092e9
ankeris <aneke1234@gmail.com>
2019-02-23 19:52:14 +0100

    last homework

---
+++ AarhusWebCorp/Controllers/ContactFormSurfaceController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+using AarhusWebCorp.ViewModels;
+
+namespace AarhusWebCorp.Controllers
+{
+    public class ContactFormSurfaceController : SurfaceController
+    {
+        // GET: ContactFormSurface
+        public ActionResult Index()
+        {
+            return PartialView("ContactForm", new ContactForm());
+        }
+    }
+}
